68c98346fe708f74fff9529d05edb9e60380231f
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-02-07 16:31:15 +0100

    regional comparison ecol. indices

--- code/analysis species level.R
+++ code/analysis species level.R
@@ -25,9 +25,9 @@ for (group in c(group.names, "all groups")){
   Mes_sr = rarefyTaxRichness(mySample = Mes, subsampleTo = subsampleTo, noOfRep = noOfRep)
   Zan_sr = rarefyTaxRichness(mySample = Tor, subsampleTo = subsampleTo, noOfRep = noOfRep)
   file_name = paste0("figs/sr_through_time/sr_through_time_whole_basin_",group,".pdf")
-  ylab = "Species Richness"
   main = paste0("Species Richness ", group, " whole basin")
   ylim = c(0, max(c(Tor_sr, Mes_sr, Zan_sr)))
+  ylab = paste0("Species richness \n subsampled to ",  subsampleTo, " Occurrences")
   pdf(file = file_name)
   boxplot(list( "Tortonian" = Tor_sr,
                 "Messinian" = Mes_sr,
@@ -51,16 +51,69 @@ for (group in c(group.names, "all groups")){
     file_name = paste0("figs/eco_timeslice_comp/",group, "_", ind,"_whole_basin.pdf")
     pdf(file = file_name)
     main = paste0(ind, " for " , group, " whole basin")
+    ylab = paste0(ind, "\n subsampled to ",  subsampleTo, " Occurrences")
     boxplot(list("T vs. M" = TM[[ind]],
                  "M vs. Z" = MZ[[ind]],
                  "T vs. Z" = TZ[[ind]]),
             ylim = c(0,1),
-            ylab = ind,
-            main = main)
+            main = main,
+            ylab = ylab,
+            mar = c(5,5,1,1))
     dev.off()
   }
 }
 
+#### Species richness through time for all groups in all regions ####
+for (group in c(group.names, "all groups")){
+  for (reg in regions){
+    Tor = get_from_db(group = group, basin = reg, timeslice = "Tortonian" )
+    Mes = get_from_db(group = group, basin = reg, timeslice = "pre-evaporitic Messinian" )
+    Zan = get_from_db(group = group, basin = reg, timeslice = "Zanclean" )
+    subsampleTo = ceiling(0.8 * (min(c(length(Tor), length(Mes), length(Zan)))))
+    Tor_sr = rarefyTaxRichness(mySample = Tor, subsampleTo = subsampleTo, noOfRep = noOfRep)
+    Mes_sr = rarefyTaxRichness(mySample = Mes, subsampleTo = subsampleTo, noOfRep = noOfRep)
+    Zan_sr = rarefyTaxRichness(mySample = Tor, subsampleTo = subsampleTo, noOfRep = noOfRep)
+    file_name = paste0("figs/sr_through_time_regional/sr_through_time_", group, "_",  reg ,".pdf")
+    ylab = "Species Richness"
+    main = paste0("Species Richness ", group, " ", reg)
+    ylim = c(0, max(c(Tor_sr, Mes_sr, Zan_sr)))
+    pdf(file = file_name)
+    boxplot(list( "Tortonian" = Tor_sr,
+                  "Messinian" = Mes_sr,
+                  "Zanclean" =  Zan_sr),
+            ylim = ylim,
+            ylab = ylab,
+            main = main)
+    dev.off()
+    }
+}
+
+#### Ecological indices through time per region ( all groups) ####
+for (group in c(group.names, "all groups")){
+  for (reg in regions){
+    Tor = get_from_db(group = group, basin = "whole basin", timeslice = "Tortonian" )
+    Mes = get_from_db(group = group, basin = "whole basin", timeslice = "pre-evaporitic Messinian" )
+    Zan = get_from_db(group = group, basin = "whole basin", timeslice = "Zanclean" )
+    subsampleTo = ceiling(0.8 * (min(c(length(Tor), length(Mes), length(Zan)))))
+    TM = rarefyEcoIndexes(Tor, Mes, subsampleTo, noOfRep)
+    MZ = rarefyEcoIndexes(Mes, Zan, subsampleTo, noOfRep)
+    TZ = rarefyEcoIndexes(Tor, Zan, subsampleTo, noOfRep)
+    for (ind in c("soerensen", "simpson", "nestedness")){
+      file_name = paste0("figs/eco_timeslice_comp_regional/",group, "_", reg ,"_", ind, ".pdf")
+      pdf(file = file_name)
+      main = paste0(ind, " for " , group, " ", reg)
+      ylab = paste0(ind, "\n subsampled to ",  subsampleTo, " Occurrences")
+      boxplot(list("T vs. M" = TM[[ind]],
+                   "M vs. Z" = MZ[[ind]],
+                   "T vs. Z" = TZ[[ind]]),
+              ylim = c(0,1),
+              main = main,
+              ylab = ylab,
+              mar = c(5,5,1,1))
+      dev.off()
+    }
+  }
+}
 
 #### eco indexes geographic comparison through time ####
 ## get sample isze
