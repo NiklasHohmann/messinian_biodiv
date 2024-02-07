45044261bdc734c0abcc7f29f3f8202f465cdc58
Niklas Hohmann <67792281+NiklasHohmann@users.noreply.github.com>
2024-02-07 15:14:40 +0100

    + analysis

--- code/analysis species level.R
+++ code/analysis species level.R
@@ -1,151 +1,95 @@
-# load DB first
+#### load data ####
 messinian_db <- read.csv(file = "data/messinianDB.csv")
 
-#### Define constants ####
-timebins <- unique(messinian_db$Age)[c(3, 1, 2)] # sorted from old to young
-regions <- unique(messinian_db$region.new)
-group.names <- unique(messinian_db$group.name)
-
+#### Replace invalid tax names with NA ####
 messinian_db$Species.name = replace(messinian_db$Species.name, messinian_db$Species.name == "sp.", NA)
 messinian_db$Genus.name = replace(messinian_db$Genus.name, messinian_db$Genus.name == "indet.", NA)
 messinian_db = messinian_db[ !messinian_db$Family == "indet.",  ]
 
-#### Aux Functions
-{
-MSCSample=function(group, basin, timeslice, taxLevel = "species"){
-  #'
-  #' @title extract occurrence from Messinian database
-  #' 
-  #' @description
-    #' returns a vector of taxon names from the Messinian DB on the specified taxonomic level
-    #' 
-  #' 
-  #' @param taxLevel "species","genus" or "family". Taxonomic level to extract
-  #' @param group character, element of _group.names_ or "all groups". Taxonomic groups to retreive
-  #' @param basin character, element of _regions_ or "whole basin". From which area occurrences are selected
-  #' @param timeslice character, element of _timebins_ or "all timeslices". Time interval of interest
-  #' 
-  #' @returns character vector of taxon names on the specified taxonomic level
-  
-  #### groups 
-  stopifnot(group %in% c('all groups',group.names))
-  if (group=='all groups'){
-    groupIndex=rep(TRUE,length(messiDB$group.name))
-  }
-  else{
-    groupIndex=messiDB$group.name==group
-  }
-  #### timeslices
-  stopifnot(timeslice %in% c('all timeslices', timebins))
-  if (timeslice == "all timeslices"){
-    timesliceIndex=rep(TRUE,length(messiDB$Age))
-  }
-  else {
-    timesliceIndex=messiDB$Age==timeslice
-  }
-  #### basisn
-  stopifnot(basin %in% c("whole basin",regions))
-  if (basin =="whole basin"){
-    basinIndex=rep(TRUE,length(messiDB$region.new))
-  }
-  else {
-    basinIndex=messiDB$region.new==basin
-  }
+#### Constants ####
+timebins <- unique(messinian_db$Age)[c(3, 1, 2)] # sorted from old to young
+regions <- unique(messinian_db$region.new)
+group.names <- unique(messinian_db$group.name)
+noOfRep = 100
 
-  #### taxonomic level 
-  stopifnot(taxLevel %in% c("species","genus","family"))
-  if (taxLevel=="species"){
-    taxIndex=!is.na(messiDB$Species.name) & !is.na(messiDB$Genus.name) & !is.na(messiDB$Family)
-    occ=paste(messiDB$Genus.name, messiDB$Species.name, sep=' ')
-    occ=occ[taxIndex ==TRUE & basinIndex==TRUE & timesliceIndex==TRUE & groupIndex==TRUE]
-  }
-  if (taxLevel=="genus"){
-    taxIndex=!is.na(messiDB$Genus.name)  & !is.na(messiDB$Family)
-    occ=paste(messiDB$Genus.name)
-    occ=occ[taxIndex ==TRUE & basinIndex==TRUE & timesliceIndex==TRUE & groupIndex==TRUE]
-  }
-  
-  if (taxLevel=="family"){
-    taxIndex=!is.na(messiDB$Family)
-    occ=paste(messiDB$Family)
-    occ=occ[taxIndex ==TRUE & basinIndex==TRUE & timesliceIndex==TRUE & groupIndex==TRUE]
-  }
-  return(occ)
+#### Load helper functions ####
+source("code/helper_functions.R")
+
+#### Species richness through time (whole basin, all groups) ####
+for (group in c(group.names, "all groups")){
+  Tor = get_from_db(group = group, basin = "whole basin", timeslice = "Tortonian" )
+  Mes = get_from_db(group = group, basin = "whole basin", timeslice = "pre-evaporitic Messinian" )
+  Zan = get_from_db(group = group, basin = "whole basin", timeslice = "Zanclean" )
+  subsampleTo = ceiling(0.8 * (min(c(length(Tor), length(Mes), length(Zan)))))
+  Tor_sr = rarefyTaxRichness(mySample = Tor, subsampleTo = subsampleTo, noOfRep = noOfRep)
+  Mes_sr = rarefyTaxRichness(mySample = Mes, subsampleTo = subsampleTo, noOfRep = noOfRep)
+  Zan_sr = rarefyTaxRichness(mySample = Tor, subsampleTo = subsampleTo, noOfRep = noOfRep)
+  file_name = paste0("figs/sr_through_time/sr_through_time_whole_basin_",group,".pdf")
+  ylab = "Species Richness"
+  main = paste0("Species Richness ", group, " whole basin")
+  ylim = c(0, max(c(Tor_sr, Mes_sr, Zan_sr)))
+  pdf(file = file_name)
+  boxplot(list( "Tortonian" = Tor_sr,
+                "Messinian" = Mes_sr,
+                "Zanclean" =  Zan_sr),
+          ylim = ylim,
+          ylab = ylab,
+          main = main)
+  dev.off()
 }
 
-# test
-MSCSample(taxLevel = "species",
-          group="all groups",
-          timeslice = "all timeslices",
-          basin = "whole basin")
-
-rarefyTaxRichness=function(mySample, subsampleTo=1,noOfRep=1000){
-  stopifnot(length(mySample)>=subsampleTo)
-  stopifnot(!is.na(mySample))
-  taxRichness=sapply(seq_len(noOfRep), function(x) length(unique(sample(mySample,size=subsampleTo,replace=FALSE))))
-  return(taxRichness)
-}
-
-rarefyEcoIndexes=function(mySample1, mySample2, subsampleTo=1, noOfRep=1000){
-  #' 
-  #' @title pairwise rarefaction for ecological parameters
-  #' 
-  #' @param mySample1 first vector of taxon names
-  #' @param mySample2 second vector of taxon names
-  #' @param subsampleto target sample size for subsampling.
-  #' @param noOfRep integer, number of subsampling repetitions
-  #' 
-  #' @description
-    #' performs pairwise subsampling from two vectors of taxon names, and returns 
-    #' key ecological indices (soerensen & simpson index, nestedness)
-  #'
-  #' @returns a list with three names elements: "soerensen", "simpson", "nestedness", each a vector of length _noOfRep_, containing the ecological indices of the i-th subsampling run 
-  stopifnot(length(mySample1)>=subsampleTo & length(mySample2)>=subsampleTo)
-  stopifnot(!is.na(c(mySample1,mySample2)))
-  
-  out=list(soerensen=numeric(),simpson=numeric(),nestedness=numeric())
-  for (i in 1:noOfRep){
-    selectedocc1=sample(mySample1,size=subsampleTo,replace=FALSE)
-    selectedocc2=sample(mySample2,size=subsampleTo,replace=FALSE)
-    a=length(intersect(selectedocc1,selectedocc2))
-    b=length(setdiff(selectedocc1,selectedocc2))
-    c=length(setdiff(selectedocc2,selectedocc1))
-    out$soerensen[i]=(b+c)/(2*a+b+c) # Baslega 2010
-    out$simpson[i]=min(c(b,c))/(a+min(c(b,c)))
-    out$nestedness[i]=(b+c)/(2*a+b+c)-(min(c(b,c))/(a+min(c(b,c))))
+#### Ecological indices through time (whole basin, all groups) ####
+for (group in c(group.names, "all groups")){
+  Tor = get_from_db(group = group, basin = "whole basin", timeslice = "Tortonian" )
+  Mes = get_from_db(group = group, basin = "whole basin", timeslice = "pre-evaporitic Messinian" )
+  Zan = get_from_db(group = group, basin = "whole basin", timeslice = "Zanclean" )
+  subsampleTo = ceiling(0.8 * (min(c(length(Tor), length(Mes), length(Zan)))))
+  TM = rarefyEcoIndexes(Tor, Mes, subsampleTo, noOfRep)
+  MZ = rarefyEcoIndexes(Mes, Zan, subsampleTo, noOfRep)
+  TZ = rarefyEcoIndexes(Tor, Zan, subsampleTo, noOfRep)
+  for (ind in c("soerensen", "simpson", "nestedness")){
+    file_name = paste0("figs/eco_timeslice_comp/",group, "_", ind,"_whole_basin.pdf")
+    pdf(file = file_name)
+    main = paste0(ind, " for " , group, " whole basin")
+    boxplot(list("T vs. M" = TM[[ind]],
+                 "M vs. Z" = MZ[[ind]],
+                 "T vs. Z" = TZ[[ind]]),
+            ylim = c(0,1),
+            ylab = ind,
+            main = main)
+    dev.off()
   }
-  return(out)
 }
 
-}
 
 #### eco indexes geographic comparison through time ####
 ## get sample isze
 samplesize=c()
 for (timebin in timebins){
   for (basin in regions){
-    samplesize=c(samplesize,length(MSCSample(taxLevel='species',group='all groups',basin=basin,timeslice=timebin)))
+    samplesize=c(samplesize,length(get_from_db(taxLevel='species',group='all groups',basin=basin,timeslice=timebin)))
   }
 }
-subsampleTo=floor(0.8*min(samplesize))
+
+subsampleTo=ceiling(0.8*min(samplesize))
 n=5000
 for (timeslice in timebins){
-  Eas=MSCSample(taxLevel='species',
+  Eas=get_from_db(taxLevel='species',
                 group="all groups",
                 basin="Eastern Mediterranean",
                 timeslice=timeslice)
-  PoP=MSCSample(taxLevel='species',
+  PoP=get_from_db(taxLevel='species',
                 group="all groups",
                 basin="Po Plain-Northern Adriatic",
                 timeslice=timeslice)
-  Wes=MSCSample(taxLevel='species',
+  Wes=get_from_db(taxLevel='species',
                 group="all groups",
                 basin="Western Mediterranean",
                 timeslice=timeslice)
   EasVsPoP=rarefyEcoIndexes(Eas,PoP,subsampleTo,noOfRep = n)
   PoPVsWes=rarefyEcoIndexes(PoP,Wes,subsampleTo,noOfRep = n)
   WesVsEas=rarefyEcoIndexes(Wes,Eas,subsampleTo,noOfRep = n)
-  pdf(paste("Soerensen " ,timeslice ,", all groups, Species level, comparison between regions.pdf"))
+  pdf(paste("figs/", "Soerensen " ,timeslice ,", all groups, Species level, comparison between regions.pdf"))
   boxplot(list("East vs. Po Plain"=EasVsPoP$soerensen,
                "Po Plain vs. West"=PoPVsWes$soerensen,
                "West vs. East"=WesVsEas$soerensen),
@@ -153,7 +97,7 @@ for (timeslice in timebins){
           ylab=paste("Subsampled to", subsampleTo, "occurrences"))
   dev.off()
   
-  pdf(paste("Simpson " ,timeslice ,", all groups, Species level, comparison between regions.pdf"))
+  pdf(paste("figs/", "Simpson " ,timeslice ,", all groups, Species level, comparison between regions.pdf"))
   boxplot(list("East vs. Po Plain"=EasVsPoP$simpson,
                "Po Plain vs. West"=PoPVsWes$simpson,
                "West vs. East"=WesVsEas$simpson),
@@ -161,7 +105,7 @@ for (timeslice in timebins){
           ylab=paste("Subsampled to", subsampleTo, "occurrences"))
   dev.off()
   
-  pdf(paste("Nestedness " ,timeslice ,", all groups, Species level, comparison between regions.pdf"))
+  pdf(paste("figs/", "Nestedness " ,timeslice ,", all groups, Species level, comparison between regions.pdf"))
   boxplot(list("East vs. Po Plain"=EasVsPoP$nestedness,
                "Po Plain vs. West"=PoPVsWes$nestedness,
                "West vs. East"=WesVsEas$nestedness),
@@ -175,15 +119,15 @@ for (timeslice in timebins){
 #### species richenss
 regions=c("Eastern Mediterranean","Po Plain-Northern Adriatic","Western Mediterranean")
 for (timeslice in timebins){
-  Eas=MSCSample(taxLevel='species',
+  Eas=get_from_db(taxLevel='species',
                 group="all groups",
                 basin="Eastern Mediterranean",
                 timeslice=timeslice)
-  PoP=MSCSample(taxLevel='species',
+  PoP=get_from_db(taxLevel='species',
                 group="all groups",
                 basin="Po Plain-Northern Adriatic",
                 timeslice=timeslice)
-  Wes=MSCSample(taxLevel='species',
+  Wes=get_from_db(taxLevel='species',
                 group="all groups",
                 basin="Western Mediterranean",
                 timeslice=timeslice)
@@ -199,7 +143,7 @@ for (timeslice in timebins){
                                                                      subsampleTo = subsampleTo,
                                                                      noOfRep = 50000))
   ymax=800 #max(sapply(speciesRichnessList,function(x) max(x)))
-  pdf(paste("Species_richness " ,timeslice ,", all groups, Species level.pdf"))
+  pdf(paste("figs/", "Species_richness " ,timeslice ,", all groups, Species level.pdf"))
   boxplot(speciesRichnessList,
           main=paste("Species Richness ", timeslice ,", all groups, Species level"),
           ylab=paste("Subsampled to", subsampleTo, "occurrences"),
@@ -235,7 +179,7 @@ TOME=myrarefaction(dataTo,dataPre,sampleSize,noofrep = n)
 MEZA=myrarefaction(dataPre,dataZa,sampleSize,noofrep = n)
 
 ymax=max(c(TOME$sr1,TOME$sr2,MEZA$sr2))
-pdf(paste("Species_richness (Whole Basin), all groups, Species level.pdf"))
+pdf(paste("figs/", "Species_richness (Whole Basin), all groups, Species level.pdf"))
 boxplot(list("Tortonian"=TOME$sr1,
              "pre-evaporitic Messinian"=TOME$sr2,
              "Zanclean"=MEZA$sr2 ),
@@ -244,7 +188,7 @@ boxplot(list("Tortonian"=TOME$sr1,
         ylim=c(0,1.1*ymax))
 dev.off()
 
-pdf(paste("Soerensen (Whole Basin)  all groups Species level.pdf"))
+pdf(paste("figs/", "Soerensen (Whole Basin)  all groups Species level.pdf"))
 boxplot(list("Tortonian \nvs.\npre-evo Messinian"=TOME$soerensen,
              "pre-evo Messinian\nvs.\nZanclean"=MEZA$soerensen),
         main="Soerensen (Whole Basin)   all groups  Species level",
@@ -252,7 +196,7 @@ boxplot(list("Tortonian \nvs.\npre-evo Messinian"=TOME$soerensen,
         ylim=c(0,1))
 dev.off()
 
-pdf(paste("Simpson (Whole Basin)   all groups   Species level.pdf"))
+pdf(paste("figs/", "Simpson (Whole Basin)   all groups   Species level.pdf"))
 boxplot(list("Tortonian\nvs.\npre-evo Messinian"=TOME$simpson,
              "pre-evo Messinian\nvs.\n Zanclean"=MEZA$simpson),
         main="Simpson (Whole Basin)   all groups   Species level",
@@ -260,7 +204,7 @@ boxplot(list("Tortonian\nvs.\npre-evo Messinian"=TOME$simpson,
         ylim=c(0,1))
 dev.off()
 
-pdf(paste("Nestedness (Whole Basin)  all groups  species level .pdf"))
+pdf(paste("figs/", "Nestedness (Whole Basin)  all groups  species level .pdf"))
 boxplot(list("Tortonian \nvs.\npre-evo Messinian"=TOME$nestedness,
              "pre-evo Messinian\nvs.\n Zanclean"=MEZA$nestedness),
         main="Nestedness (Whole Basin)   all groups  Species level",
@@ -281,7 +225,7 @@ TOME=myrarefaction(dataTo,dataPre,sampleSize,noofrep = n)
 MEZA=myrarefaction(dataPre,dataZa,sampleSize,noofrep = n)
 
 ymax=max(c(TOME$sr1,TOME$sr2,MEZA$sr2))
-pdf(paste("Genus_richness (Whole Basin), all groups, Genus level.pdf"))
+pdf(paste("figs/", "Genus_richness (Whole Basin), all groups, Genus level.pdf"))
 boxplot(list("Tortonian"=TOME$sr1,
              "pre-evaporitic Messinian"=TOME$sr2,
              "Zanclean"=MEZA$sr2 ),
@@ -306,7 +250,7 @@ for ( group.name in group.names){
     MEZA=myrarefaction(dataPre,dataZa,sampleSize,noofrep = n)
     
     ymax=max(c(TOME$sr1,TOME$sr2,MEZA$sr2))
-    pdf(paste("Species_richness (Whole Basin)", group.name," Species level.pdf"))
+    pdf(paste("figs/", "Species_richness (Whole Basin)", group.name," Species level.pdf"))
     boxplot(list("Tortonian"=TOME$sr1,
                  "pre-evaporitic Messinian"=TOME$sr2,
                  "Zanclean"=MEZA$sr2 ),
@@ -315,7 +259,7 @@ for ( group.name in group.names){
             ylim=c(0,1.1*ymax))
     dev.off()
     
-    pdf(paste("Soerensen (Whole Basin)  ", group.name," Species level.pdf"))
+    pdf(paste("figs/", "Soerensen (Whole Basin)  ", group.name," Species level.pdf"))
     boxplot(list("Tortonian \nvs.\npre-evo Messinian"=TOME$soerensen,
                  "pre-evo Messinian\nvs.\nZanclean"=MEZA$soerensen),
             main="Soerensen (Whole Basin)  ", group.name," Species level",
@@ -323,7 +267,7 @@ for ( group.name in group.names){
             ylim=c(0,1))
     dev.off()
     
-    pdf(paste("Simpson (Whole Basin)  ", group.name,"  Species level.pdf"))
+    pdf(paste("figs/", "Simpson (Whole Basin)  ", group.name,"  Species level.pdf"))
     boxplot(list("Tortonian\nvs.\npre-evo Messinian"=TOME$simpson,
                  "pre-evo Messinian\nvs.\n Zanclean"=MEZA$simpson),
             main="Simpson (Whole Basin)  ", group.name,"  Species level",
@@ -331,7 +275,7 @@ for ( group.name in group.names){
             ylim=c(0,1))
     dev.off()
     
-    pdf(paste("Nestedness (Whole Basin) ", group.name,"species level .pdf"))
+    pdf(paste("figs/", "Nestedness (Whole Basin) ", group.name,"species level .pdf"))
     boxplot(list("Tortonian \nvs.\npre-evo Messinian"=TOME$nestedness,
                  "pre-evo Messinian\nvs.\n Zanclean"=MEZA$nestedness),
             main="Nestedness (Whole Basin)  ", group.name," Species level",
@@ -371,28 +315,28 @@ for (timebin in timebins){
     rare_res1[[timebin]][[tax]]$WP=WP
     rare_res1[[timebin]][[tax]]$PE=PE
     rare_res1[[timebin]][[tax]]$nosamples=sampleSize
-    jpeg(paste("00fish species richness",tax, timebin, "comparison regions.jpeg"))
+    jpeg(paste("figs/", "00fish species richness",tax, timebin, "comparison regions.jpeg"))
     boxplot(list(Eastern=rare_res1[[timebin]][[tax]]$EW$sr1,
                  Western=rare_res1[[timebin]][[tax]]$WP$sr1,
                  Poplain=rare_res1[[timebin]][[tax]]$PE$sr1),
             main=paste("species richness",tax, timebin))
     dev.off()
     
-    jpeg(paste("00fish soerensen",tax, timebin, "comparison regions.jpeg"))
+    jpeg(paste("figs/", "00fish soerensen",tax, timebin, "comparison regions.jpeg"))
     boxplot(list(EvsW=rare_res1[[timebin]][[tax]]$EW$soerensen,
                  WvsP=rare_res1[[timebin]][[tax]]$WP$soerensen,
                  PvsE=rare_res1[[timebin]][[tax]]$PE$soerensen),
             main=paste("Soerensen",tax, timebin))
     dev.off()
     
-    jpeg(paste("00fish simpson",tax, timebin, "comparison regions.jpeg"))
+    jpeg(paste("figs/", "00fish simpson",tax, timebin, "comparison regions.jpeg"))
     boxplot(list(EvsW=rare_res1[[timebin]][[tax]]$EW$simpson,
                  WvsP=rare_res1[[timebin]][[tax]]$WP$simpson,
                  PvsE=rare_res1[[timebin]][[tax]]$PE$simpson),
             main=paste("Simpson",tax, timebin))
     dev.off()
     
-    jpeg(paste("00fish nestedness",tax, timebin, "comparison regions.jpeg"))
+    jpeg(paste("figs/", "00fish nestedness",tax, timebin, "comparison regions.jpeg"))
     boxplot(list(EvsW=rare_res1[[timebin]][[tax]]$EW$nestedness,
                  WvsP=rare_res1[[timebin]][[tax]]$WP$nestedness,
                  PvsE=rare_res1[[timebin]][[tax]]$PE$nestedness),
@@ -422,26 +366,26 @@ for (region in regions){
     rare_res2[[region]][[tax]]$ZM=ZM
     rare_res2[[region]][[tax]]$MT=MT
     
-    jpeg(paste("00fish species richness",tax, region, "comparison time.jpeg"))
+    jpeg(paste("figs/", "00fish species richness",tax, region, "comparison time.jpeg"))
     boxplot(list(Tortonian=rare_res2[[region]][[tax]]$MT$sr2,
                  peMessinian=rare_res2[[region]][[tax]]$MT$sr1,
                  Zanclean=rare_res2[[region]][[tax]]$ZM$sr1 ),
             main=paste("species richness",tax, region))
     dev.off()
     
-    jpeg(paste("00fish soerensen",tax, region, "comparison time.jpeg"))
+    jpeg(paste("figs/", "00fish soerensen",tax, region, "comparison time.jpeg"))
     boxplot(list(TortonianvsppeMessinian=rare_res2[[region]][[tax]]$MT$soerensen,
                  peMessinianvsZanclean=rare_res2[[region]][[tax]]$ZM$soerensen ),
             main=paste("soerensen",tax, region))
     dev.off()
     
-    jpeg(paste("00fish simpson",tax, region, "comparison time.jpeg"))
+    jpeg(paste("figs/", "00fish simpson",tax, region, "comparison time.jpeg"))
     boxplot(list(TortonianvsppeMessinian=rare_res2[[region]][[tax]]$MT$simpson,
                  peMessinianvsZanclean=rare_res2[[region]][[tax]]$ZM$simpson ),
             main=paste("simpson",tax, region))
     dev.off()
     
-    jpeg(paste("00fish nestedness",tax, region, "comparison time.jpeg"))
+    jpeg(paste("figs/", "00fish nestedness",tax, region, "comparison time.jpeg"))
     boxplot(list(TortonianvsppeMessinian=rare_res2[[region]][[tax]]$MT$nestedness,
                  peMessinianvsZanclean=rare_res2[[region]][[tax]]$ZM$nestedness ),
             main=paste("nestedness",tax, region))
--- code/helper_functions.R
+++ code/helper_functions.R
@@ -1,3 +1,137 @@
+get_from_db=function(group, basin, timeslice, taxLevel = "species"){
+  #'
+  #' @title extract occurrence from Messinian database
+  #' 
+  #' @description
+  #' returns a vector of taxon names from the Messinian DB on the specified taxonomic level, sub basin, and time slice
+  #' 
+  #' 
+  #' @param taxLevel "species","genus" or "family". Taxonomic level to extract
+  #' @param group character, element of _group.names_ or "all groups". Taxonomic groups to retreive
+  #' @param basin character, element of _regions_ or "whole basin". From which area occurrences are selected
+  #' @param timeslice character, element of _timebins_ or "all timeslices". Time interval of interest
+  #' 
+  #' @returns character vector of taxon names on the specified taxonomic level
+  
+  #### groups 
+  stopifnot(group %in% c('all groups',group.names))
+  if (group=='all groups'){
+    groupIndex=rep(TRUE,length(messinian_db$group.name))
+  }
+  else{
+    groupIndex=messinian_db$group.name==group
+  }
+  #### timeslices
+  stopifnot(timeslice %in% c('all timeslices', timebins))
+  if (timeslice == "all timeslices"){
+    timesliceIndex=rep(TRUE,length(messinian_db$Age))
+  }
+  else {
+    timesliceIndex=messinian_db$Age==timeslice
+  }
+  #### basisn
+  stopifnot(basin %in% c("whole basin",regions))
+  if (basin =="whole basin"){
+    basinIndex=rep(TRUE,length(messinian_db$region.new))
+  }
+  else {
+    basinIndex=messinian_db$region.new==basin
+  }
+  
+  #### taxonomic level 
+  stopifnot(taxLevel %in% c("species","genus","family"))
+  if (taxLevel=="species"){
+    taxIndex=!is.na(messinian_db$Species.name) & !is.na(messinian_db$Genus.name) & !is.na(messinian_db$Family)
+    occ=paste(messinian_db$Genus.name, messinian_db$Species.name, sep=' ')
+    occ=occ[taxIndex ==TRUE & basinIndex==TRUE & timesliceIndex==TRUE & groupIndex==TRUE]
+  }
+  if (taxLevel=="genus"){
+    taxIndex=!is.na(messinian_db$Genus.name)  & !is.na(messinian_db$Family)
+    occ=paste(messinian_db$Genus.name)
+    occ=occ[taxIndex ==TRUE & basinIndex==TRUE & timesliceIndex==TRUE & groupIndex==TRUE]
+  }
+  
+  if (taxLevel=="family"){
+    taxIndex=!is.na(messinian_db$Family)
+    occ=paste(messinian_db$Family)
+    occ=occ[taxIndex ==TRUE & basinIndex==TRUE & timesliceIndex==TRUE & groupIndex==TRUE]
+  }
+  return(occ)
+}
+
+
+rarefyTaxRichness=function(mySample, subsampleTo,noOfRep){
+  #'
+  #' @title rarefy taxonomic richness
+  #' 
+  #' @description
+    #' rarefies a vector of taxon names to a specific sample size
+    #' 
+  #' @param mySample vector of taxonomic names
+  #' @param subsampleTo integer, nomber of occurrences to target for subsampling. Must be larger than length(mySample)
+  #' @param noOfRep integer, number of subsampling repetitions
+  #' 
+  #' @returns integer vector of length noOfRep, containing tax richnesses at each repetition
+  stopifnot(length(mySample)>=subsampleTo)
+  stopifnot(!is.na(mySample))
+  taxRichness=sapply(seq_len(noOfRep), function(x) length(unique(sample(mySample,size=subsampleTo,replace=FALSE))))
+  return(taxRichness)
+}
+
+rarefyEcoIndexes=function(mySample1, mySample2, subsampleTo, noOfRep){
+  #' 
+  #' @title pairwise rarefaction for ecological parameters
+  #' 
+  #' @param mySample1 first vector of taxon names
+  #' @param mySample2 second vector of taxon names
+  #' @param subsampleto target sample size for subsampling.
+  #' @param noOfRep integer, number of subsampling repetitions
+  #' 
+  #' @description
+  #' performs pairwise subsampling from two vectors of taxon names, and returns 
+  #' key ecological indices (soerensen & simpson index, nestedness)
+  #'
+  #' @returns a list with three names elements: "soerensen", "simpson", "nestedness", each a vector of length _noOfRep_, containing the ecological indices of the i-th subsampling run 
+  stopifnot(length(mySample1)>=subsampleTo & length(mySample2)>=subsampleTo)
+  stopifnot(!is.na(c(mySample1,mySample2)))
+  
+  out=list(soerensen=numeric(),simpson=numeric(),nestedness=numeric())
+  for (i in 1:noOfRep){
+    selectedocc1=sample(mySample1,size=subsampleTo,replace=FALSE)
+    selectedocc2=sample(mySample2,size=subsampleTo,replace=FALSE)
+    a=length(intersect(selectedocc1,selectedocc2))
+    b=length(setdiff(selectedocc1,selectedocc2))
+    c=length(setdiff(selectedocc2,selectedocc1))
+    out$soerensen[i]=(b+c)/(2*a+b+c) # Baslega 2010
+    out$simpson[i]=min(c(b,c))/(a+min(c(b,c)))
+    out$nestedness[i]=(b+c)/(2*a+b+c)-(min(c(b,c))/(a+min(c(b,c))))
+  }
+  return(out)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 biodiv_ind = function(v1, v2, n, no_of_rep){
   #' @title get biodiversity indices
   #' 
